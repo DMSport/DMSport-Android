fc0515dc4ccde05a0f99bdf64c0236fb764e4b78
정승훈 <102812085+Tmdhoon2@users.noreply.github.com>
2022-10-20 19:50:08 +0900

    Update MyPageFragment.kt

--- app/src/main/java/com/example/dmsport_android/MyPageFragment.kt
+++ app/src/main/java/com/example/dmsport_android/MyPageFragment.kt
@@ -11,7 +11,6 @@ class MyPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
 }
