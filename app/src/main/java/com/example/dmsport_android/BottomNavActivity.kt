d078219a74738ef5c84ba23275024852120eca9b
haeun <haeun3882@gmail.com>
2022-10-20 17:29:07 +0900

    ✨ :: add bottom navigation implement

--- app/src/main/java/com/example/dmsport_android/BottomNavActivity.kt
+++ app/src/main/java/com/example/dmsport_android/BottomNavActivity.kt
@@ -2,12 +2,37 @@ package com.example.dmsport_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.dmsport_android.databinding.ActivityBottomNavBinding
 
 class BottomNavActivity : AppCompatActivity() {
+
+    private val binding : ActivityBottomNavBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_bottom_nav)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bottom_nav)
 
+        initNavigationBar()
+    }
 
+    private fun initNavigationBar() {
+        supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout, VoteFragment()).commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.vote -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout, VoteFragment()).commitAllowingStateLoss()
+                }
+                R.id.notice -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout, NoticeFragment()).commitAllowingStateLoss()
+                }
+                R.id.myPage -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.menu_frame_layout, MyPageFragment()).commitAllowingStateLoss()
+                }
+            }
+            true
+        }
     }
 }
