a05052bf7454f4e19c6c2c9e4242aa04f29cc94e
정승훈 <102812085+Tmdhoon2@users.noreply.github.com>
2022-10-20 19:50:53 +0900

    Update VoteFragment.kt

--- app/src/main/java/com/example/dmsport_android/VoteFragment.kt
+++ app/src/main/java/com/example/dmsport_android/VoteFragment.kt
@@ -12,7 +12,6 @@ class VoteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_vote, container, false)
     }
 }
