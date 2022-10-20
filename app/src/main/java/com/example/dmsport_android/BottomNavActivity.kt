c694df26a81ac386fc7e257b0928712ec30accc6
haeun <haeun3882@gmail.com>
2022-10-20 11:44:15 +0900

    🌱 :: add bottom nav activity

---
+++ app/src/main/java/com/example/dmsport_android/BottomNavActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dmsport_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BottomNavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav)
+
+
+    }
+}
