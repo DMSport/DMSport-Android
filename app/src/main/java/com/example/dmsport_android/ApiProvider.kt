2ca5d084683c0a5780529b639b3596cb1bc27e13
Tmdhoon2 <hks026naver@gmail.com>
2022-10-21 11:31:48 +0900

    🌱 :: add ApiProvider

---
+++ app/src/main/java/com/example/dmsport_android/ApiProvider.kt
@@ -0,0 +1,12 @@
+package com.example.dmsport_android
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiProvider {
+//    val retrofit = Retrofit.Builder()
+//        .baseUrl(BASE_URL)
+//        .addConverterFactory(GsonConverterFactory.create())
+//        .build()
+//        .create(ServerApi::class.java)
+}
