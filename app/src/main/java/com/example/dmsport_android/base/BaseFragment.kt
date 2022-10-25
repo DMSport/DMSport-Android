1723ac69bc5d952395737409a5a127723b20cd5d
haeun <haeun3882@gmail.com>
2022-10-25 22:20:47 +0900

    🌱 :: add baseFragment

---
+++ app/src/main/java/com/example/dmsport_android/base/BaseFragment.kt
@@ -0,0 +1,33 @@
+package com.example.dmsport_android.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<V : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+    ) : Fragment(){
+
+    protected lateinit var binding: V
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+    }
+}
