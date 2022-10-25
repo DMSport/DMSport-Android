32445b621785c6787566e0749b38ea16178328d0
haeun <haeun3882@gmail.com>
2022-10-25 22:21:59 +0900

    🐛 :: edit voteFragment

--- app/src/main/java/com/example/dmsport_android/ui/fragment/VoteFragment.kt
+++ app/src/main/java/com/example/dmsport_android/ui/fragment/VoteFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import com.example.dmsport_android.R
+import com.example.dmsport_android.base.BaseFragment
+import com.example.dmsport_android.databinding.FragmentVoteBinding
 
 
-class VoteFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_vote, container, false)
+class VoteFragment : BaseFragment<FragmentVoteBinding>(R.layout.fragment_vote) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initEventSelected()
+    }
+
+    fun initEventSelected() {
     }
 }
