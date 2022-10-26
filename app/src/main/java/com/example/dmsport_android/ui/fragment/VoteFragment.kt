f400dbf44596d0a994f910697cc71f4155e749f4
haeun <haeun3882@gmail.com>
2022-10-26 23:18:30 +0900

    🎨 :: add cardView select

--- app/src/main/java/com/example/dmsport_android/ui/fragment/VoteFragment.kt
+++ app/src/main/java/com/example/dmsport_android/ui/fragment/VoteFragment.kt
@@ -1,24 +1,42 @@
 package com.example.dmsport_android.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import com.example.dmsport_android.R
 import com.example.dmsport_android.base.BaseFragment
 import com.example.dmsport_android.databinding.FragmentVoteBinding
 
-
 class VoteFragment : BaseFragment<FragmentVoteBinding>(R.layout.fragment_vote) {
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initEventSelected()
+        binding.voteFragment = this
+
+    }
+    fun initBadmintonSelected() {
+        binding.cvVoteBad.setBackgroundResource(R.drawable.vote_card_view_on)
+        binding.cvVoteSoc.setBackgroundResource(R.drawable.vote_card_view_off)
+        binding.cvVoteVol.setBackgroundResource(R.drawable.vote_card_view_off)
+        binding.cvVoteBas.setBackgroundResource(R.drawable.vote_card_view_off)
     }
 
-    fun initEventSelected() {
+    fun initSoccerSelected() {
+        binding.cvVoteSoc.setBackgroundResource(R.drawable.vote_card_view_on)
+        binding.cvVoteBas.setBackgroundResource(R.drawable.vote_card_view_off)
+        binding.cvVoteBad.setBackgroundResource(R.drawable.vote_card_view_off)
+        binding.cvVoteVol.setBackgroundResource(R.drawable.vote_card_view_off)
+    }
+
+    fun initBasSelected() {
+        binding.cvVoteBas.setBackgroundResource(R.drawable.vote_card_view_on)
+        binding.cvVoteBad.setBackgroundResource(R.drawable.vote_card_view_off)
+        binding.cvVoteVol.setBackgroundResource(R.drawable.vote_card_view_off)
+        binding.cvVoteSoc.setBackgroundResource(R.drawable.vote_card_view_off)
+    }
+
+    fun initVolSelected() {
+        binding.cvVoteVol.setBackgroundResource(R.drawable.vote_card_view_on)
+        binding.cvVoteSoc.setBackgroundResource(R.drawable.vote_card_view_off)
+        binding.cvVoteBas.setBackgroundResource(R.drawable.vote_card_view_off)
+        binding.cvVoteBad.setBackgroundResource(R.drawable.vote_card_view_off)
     }
 }
