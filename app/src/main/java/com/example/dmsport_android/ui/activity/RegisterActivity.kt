0acc760784ac2271df891c64d4390e03fff2bc09
Tmdhoon2 <hks026naver@gmail.com>
2022-10-29 07:12:10 +0900

    ✨ :: implement next button click function

--- app/src/main/java/com/example/dmsport_android/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/dmsport_android/ui/activity/RegisterActivity.kt
@@ -1,45 +1,49 @@
 package com.example.dmsport_android.ui.activity
 
 import android.os.Bundle
-import android.text.InputType
+import androidx.lifecycle.ViewModelProvider
 import com.example.dmsport_android.R
 import com.example.dmsport_android.base.BaseActivity
 import com.example.dmsport_android.databinding.ActivityRegisterBinding
-import com.example.dmsport_android.util.getPref
+import com.example.dmsport_android.repository.RegisterRepository
 import com.example.dmsport_android.util.putPref
+import com.example.dmsport_android.util.startIntent
+import com.example.dmsport_android.viewmodel.RegisterViewModel
+import com.example.dmsport_android.viewmodel.factory.RegisterViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>(R.layout.activity_register) {
 
+    private val registerRepository : RegisterRepository by lazy {
+        RegisterRepository()
+    }
+
+    private val registerViewModelFactory : RegisterViewModelFactory by lazy {
+        RegisterViewModelFactory(registerRepository, pref)
+    }
+
+    private val registerViewModel : RegisterViewModel by lazy {
+        ViewModelProvider(this, registerViewModelFactory).get(RegisterViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initPwReVisible()
-        initPwVisible()
-        initPwReVisible()
+        binding.registerViewModel = registerViewModel
+        registerViewModel.initPwVisible()
+        registerViewModel.initPwReVisible()
         binding.registerActivity = this
     }
-
-    fun initPwVisible() {
-        if(getPref(pref, "visiblePw", false) as Boolean){
-            binding.imgRegisterVisiblePw.setBackgroundResource(R.drawable.ic_visible_on)
-            binding.etRegisterPw.inputType = InputType.TYPE_TEXT_VARIATION_NORMAL
-            putPref(pref.edit(), "visiblePw", false)
+    
+    fun initNextButton(){
+        val name = binding.etRegisterName.text.toString()
+        val pw = binding.etRegisterPw.text.toString()
+        val pwRe = binding.etRegisterPwRe.text.toString()
+        if(name.isNotEmpty() && pw.isNotEmpty() && pwRe.isNotEmpty() && pw.equals(pwRe)){
+            putPref(pref.edit(), "name", name)
+            putPref(pref.edit(), "pw", pw)
+            startIntent(this, VerifyActivity::class.java)
         }else{
-            binding.imgRegisterVisiblePw.setBackgroundResource(R.drawable.ic_visible_off)
-            binding.etRegisterPw.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-            putPref(pref.edit(), "visiblePw", true)
+            Snackbar.make(binding.btRegisterNext, "항목을 확인해주세요!", Snackbar.LENGTH_SHORT).show()
         }
     }
-
-    fun initPwReVisible() {
-        if(getPref(pref, "visiblePwRe", false) as Boolean){
-            binding.imgRegisterVisiblePwRe.setBackgroundResource(R.drawable.ic_visible_on)
-            binding.etRegisterPwRe.inputType = InputType.TYPE_TEXT_VARIATION_NORMAL
-            putPref(pref.edit(), "visiblePwRe", false)
-        }else{
-            binding.imgRegisterVisiblePwRe.setBackgroundResource(R.drawable.ic_visible_off)
-            binding.etRegisterPwRe.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-            putPref(pref.edit(), "visiblePwRe", true)
-        }
-    }
-
 }
