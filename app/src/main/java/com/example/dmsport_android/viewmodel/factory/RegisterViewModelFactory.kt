2000c9756cd9c7e52bb01b415cb9761067c1a93c
Tmdhoon2 <hks026naver@gmail.com>
2022-10-29 07:10:35 +0900

    🌱 :: add RegisterViewModelFactory

---
+++ app/src/main/java/com/example/dmsport_android/viewmodel/factory/RegisterViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.example.dmsport_android.viewmodel.factory
+
+import android.content.SharedPreferences
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.dmsport_android.repository.RegisterRepository
+import com.example.dmsport_android.viewmodel.RegisterViewModel
+
+class RegisterviewModelFactory(
+    private val registerRepository: RegisterRepository,
+    private val sharedPreferences: SharedPreferences,
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return RegisterViewModel(registerRepository, sharedPreferences) as T
+    }
+}
