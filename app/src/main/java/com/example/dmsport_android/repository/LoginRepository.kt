2589f5b4ff903b483c77a76383537b88a3c5b447
Tmdhoon2 <hks026naver@gmail.com>
2022-11-01 22:11:57 +0900

    ✨ :: implement login logic

--- app/src/main/java/com/example/dmsport_android/repository/LoginRepository.kt
+++ app/src/main/java/com/example/dmsport_android/repository/LoginRepository.kt
@@ -1,10 +1,14 @@
 package com.example.dmsport_android.repository
 
+import android.util.Log
+import com.example.dmsport_android.dto.request.LoginRequest
+import com.example.dmsport_android.dto.response.LoginResponse
+import com.example.dmsport_android.network.ApiProvider
 import retrofit2.Response
 
 class LoginRepository {
 
-//    suspend fun login(loginRequest: LoginRequest) : Response<LoginResponse> =
-//        ApiProvider.retrofit.login(loginRequest)
-
+    suspend fun login(loginRequest: LoginRequest) : Response<LoginResponse> {
+        return ApiProvider.retrofit.login(loginRequest)
+    }
 }
