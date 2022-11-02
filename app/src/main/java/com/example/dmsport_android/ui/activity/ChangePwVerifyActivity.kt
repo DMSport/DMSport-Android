9bf26c8c05d4067f2fe67876669464bd612d256d
haeun <haeun3882@gmail.com>
2022-11-02 23:03:12 +0900

    🌱 :: add changePwVerify activity && xml

---
+++ app/src/main/java/com/example/dmsport_android/ui/activity/ChangePwVerifyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dmsport_android.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePwVerifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_pw_verify)
+    }
+}
