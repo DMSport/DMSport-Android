795284ec40720e96bd26d7803fae22c862359849
haeun <haeun3882@gmail.com>
2022-11-04 17:04:28 +0900

    ♻️ :: edit serverApi

--- app/src/main/java/com/example/dmsport_android/network/ServerApi.kt
+++ app/src/main/java/com/example/dmsport_android/network/ServerApi.kt
@@ -6,6 +6,7 @@ import com.example.dmsport_android.dto.response.RegisterResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ServerApi {
     @POST("users/auth")
@@ -32,5 +33,15 @@ interface ServerApi {
     suspend fun register(
         @Body registerRequest : RegisterRequest
     ) : Response<RegisterResponse>
+
+    @POST("users/mail/find")
+    suspend fun findVerifyEmail(
+        @Body findPwVerifyEmailRequest: FindPwVerifyEmailRequest
+    ) : Response<Void>
+
+    @PUT("users/password")
+    suspend fun emailChangePw(
+        @Body emailChangePwRequest: EmailChangePwRequest
+    ) : Response<Void>
 
 }
