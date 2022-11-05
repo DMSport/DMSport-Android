954b3857e3efd049bae2dd8fd84d1145cf80f840
Tmdhoon2 <hks026naver@gmail.com>
2022-11-05 20:19:10 +0900

    ✨ :: implement my function

--- app/src/main/java/com/example/dmsport_android/network/ServerApi.kt
+++ app/src/main/java/com/example/dmsport_android/network/ServerApi.kt
@@ -2,35 +2,42 @@ package com.example.dmsport_android.network
 
 import com.example.dmsport_android.dto.request.*
 import com.example.dmsport_android.dto.response.LoginResponse
+import com.example.dmsport_android.dto.response.MyPageResponse
 import com.example.dmsport_android.dto.response.RegisterResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ServerApi {
     @POST("users/auth")
     suspend fun login(
-        @Body loginRequest: LoginRequest
+        @Body loginRequest: LoginRequest,
     ) : Response<LoginResponse>
 
     @POST("users/mail/duplicate")
     suspend fun duplicate(
-        @Body duplicateRequest: DuplicateRequest
+        @Body duplicateRequest: DuplicateRequest,
     ) : Response<Void>
 
     @POST("users/mail/signup")
     suspend fun verifyEmail(
-        @Body verifyEmailRequest: VerifyEmailRequest
+        @Body verifyEmailRequest: VerifyEmailRequest,
     ) : Response<Void>
 
     @POST("users/mail/verify")
     suspend fun verify(
-        @Body verifyRequest: VerifyRequest
+        @Body verifyRequest: VerifyRequest,
     ) : Response<Void>
 
     @POST("users")
     suspend fun register(
-        @Body registerRequest : RegisterRequest
+        @Body registerRequest : RegisterRequest,
     ) : Response<RegisterResponse>
 
+    @GET("users/my")
+    suspend fun my(
+        @Header("Authorization") accessToken : String,
+    ) : Response<MyPageResponse>
 }
