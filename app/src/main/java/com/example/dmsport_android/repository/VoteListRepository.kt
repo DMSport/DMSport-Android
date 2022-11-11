bd320bcd677739bce2f65196fa7ca59c69f6826b
Tmdhoon2 <hks026naver@gmail.com>
2022-11-12 00:01:12 +0900

    🌱 :: VoteListRepository

---
+++ app/src/main/java/com/example/dmsport_android/repository/VoteListRepository.kt
@@ -0,0 +1,18 @@
+package com.example.dmsport_android.repository
+
+import com.example.dmsport_android.dto.response.VoteListResponse
+import com.example.dmsport_android.network.voteApi
+import com.example.dmsport_android.util.ACCESS_TOKEN
+import retrofit2.Response
+
+class VoteListRepository {
+    suspend fun getVoteList(
+        type : String,
+        date : String,
+    ) : Response<ArrayList<VoteListResponse>> =
+        voteApi.getVoteList(
+            ACCESS_TOKEN,
+            type,
+            date,
+        )
+}
