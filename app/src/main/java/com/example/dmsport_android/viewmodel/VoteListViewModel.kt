9328b2cfe883d6dd17fe57223bf9108ed0dd5608
Tmdhoon2 <hks026naver@gmail.com>
2022-11-12 00:01:36 +0900

    🌱 :: VoteListViewModel

---
+++ app/src/main/java/com/example/dmsport_android/viewmodel/VoteListViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.dmsport_android.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dmsport_android.dto.response.VoteListResponse
+import com.example.dmsport_android.repository.VoteListRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class VoteListViewModel(
+    private val voteListRepository : VoteListRepository,
+) : ViewModel(){
+
+    private val _voteListResponse : MutableLiveData<Response<ArrayList<VoteListResponse>>> = MutableLiveData()
+    val voteListResponse : LiveData<Response<ArrayList<VoteListResponse>>> = _voteListResponse
+
+    fun getVoteList(
+        type : String,
+        date : String,
+    ){
+        viewModelScope.launch(Dispatchers.IO){
+             _voteListResponse.postValue(voteListRepository.getVoteList(type, date))
+        }
+    }
+}
