b81f8c03e9569f81212f67ffd6de14eabe4dcec3
haeun <haeun3882@gmail.com>
2022-11-18 19:55:16 +0900

    🌱 :: notice repository

---
+++ app/src/main/java/com/example/dmsport_android/repository/NoticeRepository.kt
@@ -0,0 +1,10 @@
+package com.example.dmsport_android.repository
+
+import com.example.dmsport_android.dto.response.RecentNoticeResponse
+import com.example.dmsport_android.network.NoticeApi
+import com.example.dmsport_android.util.ACCESS_TOKEN
+import retrofit2.Response
+
+class NoticeRepository {
+
+}
