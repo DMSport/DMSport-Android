095b0f4c6cb70ff81b843416f4c77e4fd62bbd88
Tmdhoon2 <hks026naver@gmail.com>
2022-11-20 13:49:52 +0900

    ✨ :: implement check boolean verified logic

--- app/src/main/java/com/example/dmsport_android/viewmodel/MyPageViewModel.kt
+++ app/src/main/java/com/example/dmsport_android/viewmodel/MyPageViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.dmsport_android.dto.request.DeleteUserRequest
 import com.example.dmsport_android.dto.response.MyPageResponse
 import com.example.dmsport_android.repository.MyPageRepository
+import com.example.dmsport_android.util.isVerified
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Response
@@ -23,6 +24,10 @@ class MyPageViewModel(
 
     private val _deleteUserResponse : MutableLiveData<Response<Void>> = MutableLiveData()
     val deleteUserResponse : LiveData<Response<Void>> = _deleteUserResponse
+
+    fun initializeVerified(){
+        isVerified = !isVerified
+    }
 
     fun fetchMyPage(){
         viewModelScope.launch(Dispatchers.IO){
