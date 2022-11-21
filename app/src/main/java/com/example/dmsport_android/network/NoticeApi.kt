3435a37916b921c8652b4cff0b80f2636ea0dee0
haeun <haeun3882@gmail.com>
2022-11-21 13:43:31 +0900

    ✨ :: implement all notice

--- app/src/main/java/com/example/dmsport_android/network/NoticeApi.kt
+++ app/src/main/java/com/example/dmsport_android/network/NoticeApi.kt
@@ -1,13 +1,20 @@
 package com.example.dmsport_android.network
 
+import com.example.dmsport_android.dto.response.AllNoticeResponse
 import com.example.dmsport_android.dto.response.RecentNoticeResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 
 interface NoticeApi {
-    @GET("recent")
-    suspend fun recentNotice(
+
+    @GET("notices/recent")
+    suspend fun recent(
         @Header("Authorization") accessToken : String,
-    ) :Response<RecentNoticeResponse>
+    ): Response<RecentNoticeResponse>
+
+    @GET("notices")
+    suspend fun getAllNotice(
+        @Header("Authorization") accessToken: String,
+    ): Response<AllNoticeResponse>
 }
