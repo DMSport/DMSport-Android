f5d1745891a0b8d0b987a27fa02436a6494da7b7
Tmdhoon2 <hks026naver@gmail.com>
2022-11-25 19:13:23 +0900

    ✨ :: implement get admin notice list logic

--- app/src/main/java/com/example/dmsport_android/feature/notice/fragment/NoticeFragment.kt
+++ app/src/main/java/com/example/dmsport_android/feature/notice/fragment/NoticeFragment.kt
@@ -3,12 +3,15 @@ package com.example.dmsport_android.feature.notice.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dmsport_android.R
 import com.example.dmsport_android.base.BaseFragment
 import com.example.dmsport_android.databinding.FragmentNoticeBinding
 import com.example.dmsport_android.feature.notice.viewmodel.NoticeViewModel
 import com.example.dmsport_android.feature.notice.viewmodel.factory.NoticeViewModelFactory
 import com.example.dmsport_android.feature.notice.activity.MoreAllNoticeActivity
+import com.example.dmsport_android.feature.notice.adapter.AdminNoticeAdapter
+import com.example.dmsport_android.feature.notice.model.Admin
 import com.example.dmsport_android.feature.vote.repository.NoticeRepository
 import com.example.dmsport_android.util.startIntent
 
@@ -24,17 +27,45 @@ class NoticeFragment : BaseFragment<FragmentNoticeBinding>(R.layout.fragment_not
     }
 
     private val noticeViewModel: NoticeViewModel by lazy {
-        ViewModelProvider(this, noticeViewModelFactory).get(NoticeViewModel::class.java)
+        ViewModelProvider(
+            this,
+            noticeViewModelFactory,
+        ).get(NoticeViewModel::class.java)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(
+            view,
+            savedInstanceState,
+        )
         binding.viewmodel = noticeViewModel
-
-        binding.fragment = this
+        moreAllNotice()
+        observeAdminNoticeResponse()
+        noticeViewModel.getRecentNoticeList()
     }
 
     fun moreAllNotice() {
-        startIntent(this.requireContext(), MoreAllNoticeActivity::class.java)
+        binding.tvNoticeMoreAll.setOnClickListener {
+            startIntent(this.requireContext(), MoreAllNoticeActivity::class.java)
+        }
     }
+
+    private fun observeAdminNoticeResponse(){
+        noticeViewModel.recentNoticeResponse.observe(viewLifecycleOwner){
+            when(it.code()){
+                200-> initAdminNoticeRecyclerView(it.body()!!.admin)
+            }
+        }
+    }
+
+    private fun initAdminNoticeRecyclerView(adminNoticeList : ArrayList<Admin>){
+        binding.rvNoticeAdmin.run {
+            adapter = AdminNoticeAdapter(adminNoticeList)
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
 }
--- app/src/main/java/com/example/dmsport_android/feature/notice/model/RecentNoticeResponse.kt
+++ app/src/main/java/com/example/dmsport_android/feature/notice/model/RecentNoticeResponse.kt
@@ -1,11 +1,19 @@
 package com.example.dmsport_android.feature.notice.model
 
 data class RecentNoticeResponse(
-    val manager: String,
-    val admin: String,
+    val manager: ArrayList<Manager>,
+    val admin: ArrayList<Admin>,
 )
 
-data class Recent(
+data class Manager(
+    val id: Int,
+    val title: String,
+    val content_preview: String,
+    val type: String,
+    val created_at: String,
+)
+
+data class Admin(
     val id: Int,
     val title: String,
     val content_preview: String,
